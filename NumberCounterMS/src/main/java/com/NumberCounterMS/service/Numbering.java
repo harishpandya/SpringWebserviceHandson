2a10c44fe1b04111bc7ad812e7a7428034d4e4dc
harishpandya <harishpandya20@gmail.com>
2020-02-01 18:56:21 +0000

    updated with spring cloud config

--- NumberCounterMS/src/main/java/com/NumberCounterMS/service/Numbering.java
+++ NumberCounterMS/src/main/java/com/NumberCounterMS/service/Numbering.java
@@ -2,9 +2,13 @@ package com.NumberCounterMS.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 
@@ -30,4 +34,25 @@ return 1;
         System.out.println(" done in the method");
         return i;
     }
+    @GetMapping("/count/{number}")
+    public String getCounting(@PathVariable int number) {
+        List string = new ArrayList();
+
+        for (int i=1; i<number;i++ ) {
+
+            string.add(restTemplate.getForObject("http://ONE/one", Integer.class))  ;
+            string.add(restTemplate.getForObject("http://TWO/two", Integer.class) );
+            string.add(restTemplate.getForObject("http://THREE/three", Integer.class));
+            string.add(restTemplate.getForObject("http://FOUR/four", Integer.class) );
+            string.add(restTemplate.getForObject("http://FIVE/five", Integer.class) );
+            string.add(restTemplate.getForObject("http://SIX/six", Integer.class) );
+            string.add(restTemplate.getForObject("http://SEVEN/seven", Integer.class)) ;
+            string.add(restTemplate.getForObject("http://EIGHT/eight", Integer.class) );
+            string.add(restTemplate.getForObject("http://NINE/nine", Integer.class) );
+        }
+
+
+        return "String total count -> "+ string.size()+"/n "+string.toString();
+    }
+
 }
